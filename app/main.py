09d970754c9ef90689a68aa4c0ceb9329ff79e2c
gabriel <gabrielsrosas@gmail.com>
2025-11-21 17:39:54 -0300

    add redis and celery

---
+++ app/main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI,File,UploadFile
+from celery import Celery
+import time
+import whisper
+
+app = FastAPI()
+
+celery_app = Celery(
+    'worker',
+    broker='redis://redis:6379/0',
+    backend='redis://redis:6379/0'
+)
+
+model = whisper.load_model("base")
+
+@app.get("/")
+def read_root():
+    return {"message": "Docker está funcionando! O ambiente está pronto."}
+
+@app.post("/")
+def post_root(myFile: UploadFile):
+    path = f"uploads/{myFile.filename}"
+
+    with open(path,"wb") as f:
+        f.write(myFile.file.read())
+
+    result = process.delay(path)
+    return {"id" : result.id}
+
+@app.get("/result/{taskId}")
+def request_status(taskId: str):
+    task = celery_app.AsyncResult(taskId)
+    return {
+        "state" : task.state,
+        "result" : task.result
+    }
+
+@celery_app.task
+def process(filePath: str):
+    print(f"INICIANDO transcrição do arquivo: {filePath}", flush=True)
+    result = model.transcribe(filePath)
+    print("✅ FIM da transcrição. Gerando texto...", flush=True)
+    transcription = result["text"].lower()
+    return {"transcription" : transcription}
